f1084c3e26694a4ef1ffca4a094e618e151259c3
Davi Paula <dpaula@wayfair.com>
2021-04-06 12:33:53 +0200

    Adding additional config for frontend

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
+
 case $1 in
   start)
     # The '| cat' is to trick Node that this is an non-TTY terminal
